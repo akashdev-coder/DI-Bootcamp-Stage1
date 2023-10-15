5fbe4ae4f600ce0eea60a35102bc4edafcab57c9
Akash <akash.aubeeluck1@gmail.com>
2023-10-15 14:30:27 +0400

    Final Project Bootcamp Prep

---
+++ head.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    // Load header content
+    $("header").load("./header/navbar.html");
+
+});
